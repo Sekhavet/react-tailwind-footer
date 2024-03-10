2b364c4ebb8a6dcf80963a14e7735b8a5b6e0c6a
Sekhavet <102406346+Sekhavet@users.noreply.github.com>
2024-03-10 20:06:56 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+  SlSocialYoutube,
+  SlSocialFacebook,
+  SlSocialInstagram,
+} from "react-icons/sl";
+
+function App() {
+  return (
+    <>
+      <p className="p-6">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda ad veritatis nulla temporibus quam corrupti nam iure, molestias eaque quidem ipsam quae nostrum sit adipisci. Maiores, placeat. Optio aspernatur rem doloribus facere dignissimos dolores pariatur ipsa. Nulla sunt voluptates ratione illo magnam reprehenderit soluta, architecto minus neque perspiciatis, eum eius eos reiciendis? Qui, beatae est! Reiciendis at dicta sit similique beatae unde distinctio, facilis saepe reprehenderit sed eaque maiores officiis aut alias quisquam quo. Veniam sit ab distinctio aperiam! Iste ea praesentium at accusamus dicta illum adipisci modi, exercitationem cupiditate ratione unde incidunt, quas rerum, quibusdam eius nihil ullam in quia ipsum culpa laborum sit aperiam. Adipisci amet deleniti provident architecto quae sed iure, asperiores, rem obcaecati laudantium, eveniet cumque nesciunt placeat esse delectus fuga eos. Cupiditate praesentium rem corporis labore, nihil tenetur ea, provident in optio culpa temporibus magni veniam natus perspiciatis placeat sequi sint non? Repellat iusto, perferendis, iure ab sunt repellendus odit explicabo iste rem voluptates nemo molestias vel consequuntur quisquam deserunt sit? Commodi placeat quaerat dicta praesentium dignissimos reprehenderit, optio et architecto? Temporibus enim deserunt officiis similique ea repudiandae consequuntur debitis a? Repudiandae dicta aperiam ab nostrum beatae totam ratione libero explicabo accusantium unde itaque iste impedit vitae repellat, molestiae distinctio et at autem dolorem quod voluptates nemo, odio quae cumque. Quisquam obcaecati adipisci quidem quasi recusandae impedit voluptates distinctio, dolorum consequatur necessitatibus fuga, ipsum praesentium enim voluptas est autem laudantium! Accusantium id placeat tempora dolor tenetur nesciunt soluta similique corporis quae vitae, ex quibusdam iure voluptatibus maiores amet consectetur dolore hic eligendi nisi ducimus perferendis ipsum cum quod laborum? Nemo maiores corporis, sapiente eos molestiae aspernatur ratione quibusdam esse ex? Non eligendi nobis consequuntur aspernatur eius similique obcaecati eos sed nihil iste placeat unde illo sequi, iusto quis est impedit, ullam corrupti aperiam dolores nam tempore ea esse! Quasi facilis enim voluptatem atque nesciunt architecto fugit explicabo voluptatum dignissimos illum. Sapiente nihil illo iusto officia nostrum recusandae earum illum dolores sequi incidunt ea, ex eum? Labore expedita nostrum, et obcaecati blanditiis cupiditate totam voluptatum minus sed laborum quibusdam iste excepturi itaque? Reiciendis dolorem sed est. Labore impedit delectus aperiam est nesciunt amet iste molestias. Placeat, ea deleniti amet ipsa aliquid atque, facilis consectetur, sit non magnam quidem odit recusandae nemo est doloribus eaque ratione distinctio nesciunt repellat. Sequi esse aspernatur dicta beatae minima distinctio cumque, veniam cupiditate illum repellat magni provident tempore doloremque. Odio optio pariatur totam est aliquid cupiditate velit quae sapiente distinctio maxime, aut ipsam iste unde, culpa ullam delectus. Quas quo ab, atque voluptate deserunt dolorum eius nisi at dignissimos in. Itaque officia similique explicabo deleniti neque corrupti deserunt porro, aspernatur, nemo iste quibusdam nulla natus velit quisquam molestiae? Repellendus deserunt aspernatur fugiat libero veniam eius totam rem sit. Corporis distinctio inventore accusamus voluptatum quasi eius excepturi nesciunt eligendi repudiandae. Laudantium eveniet suscipit illo fugit blanditiis quaerat, dolorem eos, accusantium aspernatur recusandae nostrum harum? Illum in qui similique quis reprehenderit mollitia, eaque obcaecati, excepturi itaque delectus nisi? Tenetur animi illum illo culpa eos iure doloremque nisi fuga ipsa optio nobis adipisci autem architecto, corporis dolorem harum eligendi officia? Hic numquam possimus voluptatem. Provident eveniet exercitationem accusantium facere, rerum natus reprehenderit autem atque, recusandae dolorum asperiores cumque illo et minima quisquam a ratione libero. Eveniet inventore voluptas totam debitis eligendi quam perferendis! Tenetur, voluptatem! Praesentium numquam quod modi exercitationem et, laudantium cumque quis eaque soluta ea sequi officiis suscipit nobis natus labore amet minima repellendus, maiores perspiciatis voluptatibus distinctio. Aut, laborum possimus voluptatibus tempore debitis eius fugiat unde qui quia nemo. Non dolores in itaque quo reprehenderit, repudiandae eos perferendis dolore est rem iusto eius officia quidem et atque tenetur! Nesciunt sed nulla reiciendis vitae libero nostrum alias obcaecati. Molestiae a tenetur excepturi nisi mollitia qui voluptatibus obcaecati neque ad labore! Doloribus, iusto autem dicta officia cupiditate repellendus non deserunt totam ipsa deleniti! Aperiam ipsam officia aliquam minus repellat, omnis expedita. Culpa minus minima provident impedit eum atque tempore repudiandae doloremque eligendi, magnam sit distinctio ab eos earum ipsam harum molestiae, in recusandae voluptatum cumque quidem perspiciatis quisquam! Labore nisi quis fuga, commodi adipisci, repellat voluptatibus doloremque minus quidem veritatis velit repudiandae aliquid magni atque. Rem neque cum culpa deleniti sint, voluptatibus voluptatem, ex mollitia laudantium facilis dignissimos. Voluptatibus laudantium nam error impedit deleniti reiciendis placeat explicabo nulla nemo saepe vel sapiente ipsa libero harum, laboriosam velit accusamus quae quo recusandae quisquam eveniet? Neque saepe corporis beatae, qui impedit vel animi id aliquam sapiente. Repudiandae expedita nobis voluptas quae voluptatum placeat quis laboriosam inventore cupiditate. Ad vitae ratione impedit labore earum fugiat odio quae est! Debitis maiores alias velit illo necessitatibus neque laborum mollitia, nemo, illum deleniti est rerum molestias quidem quisquam incidunt aut. Aliquam natus fugit ipsa nostrum, commodi maiores quis necessitatibus nulla, sint reprehenderit doloremque nisi tempore cupiditate officia voluptates magni veritatis harum soluta optio incidunt eos alias perferendis dolorem perspiciatis? Quasi impedit nobis nemo illo ipsa blanditiis autem animi perferendis doloribus assumenda consectetur cum aut, possimus exercitationem earum vero recusandae voluptates odit explicabo. Voluptates optio ipsam cupiditate reiciendis corporis tenetur iste ab cum? Distinctio consequatur saepe delectus ducimus illo! Ab accusantium magni rerum! Dignissimos recusandae maiores libero excepturi laboriosam maxime. Iste sed magnam quibusdam nulla! Doloribus quo harum tempore dignissimos eum quaerat ad odio. Optio, tempora rerum! Sed maxime exercitationem reiciendis aut commodi totam laborum, recusandae iusto cumque excepturi aperiam quod vitae? Quasi dicta ut adipisci. Consequatur, molestias nobis. Et eius perferendis quis libero necessitatibus mollitia nam exercitationem? Facere delectus consequatur molestiae natus nam ullam tempore, debitis facilis sit totam praesentium velit doloremque, nostrum id asperiores officiis cumque accusantium aut ratione. Sed quidem quod aut asperiores, incidunt consequatur? Facere, voluptatem, error ex neque obcaecati libero iusto repellat repudiandae aut hic aspernatur, quidem perspiciatis eaque maiores aperiam sequi assumenda. Rem veniam beatae dolor mollitia, doloribus officiis aut iusto eum et esse ea fuga, unde animi consectetur quidem vitae nostrum voluptatibus sunt est obcaecati dolore exercitationem sapiente odio magnam? Unde dolorem error modi, veritatis, illum repellendus nam incidunt alias, ab doloremque itaque?
+      </p>
+      <div className="relative bg-slate-600 p-10 rounded-t-xl bottom-0 w-full">
+        <div className="flex justify-between text-white">
+          <div className="font-bold text-lg flex flex-col">
+            Products
+            <ul className="font-semibold text-sm opacity-60">
+              <li>Visual studio X</li>
+              <li>Easy content</li>
+              <li>Drag and drop</li>
+            </ul>
+          </div>
+          <div className="font-bold text-lg flex flex-col">
+            Resources
+            <ul className="font-semibold text-sm opacity-60">
+              <li>Online evenet</li>
+              <li>Blog</li>
+              <li>Use cases</li>
+            </ul>
+          </div>
+          <div className="font-bold text-lg flex flex-col">
+            Company
+            <ul className="font-semibold text-sm opacity-60">
+              <li>Press</li>
+              <li>Customer stories</li>
+              <li>Online communities</li>
+              <li>About us</li>
+            </ul>
+          </div>
+          <div className="font-bold text-lg flex flex-col">
+            Support
+            <ul className="font-semibold text-sm opacity-60">
+              <li>Documentation</li>
+              <li>Tutorials & guides</li>
+              <li>Webinars</li>
+              <li>Open-source</li>
+            </ul>
+          </div>
+        </div>
+        <hr className="my-6 opacity-40" />
+        <div className="flex justify-between mx-4 font-semibold text-white opacity-70">
+          <div>
+            <p>© 2024 Appy. All rights reserved.</p>
+          </div>
+          <div>
+            <p>Terms - Privacy & Policy</p>
+          </div>
+          <div>
+            <ul className="font-bold text-xl flex gap-4">
+              <li className="hover:bg-blue-400 p-[6px] rounded-full">
+                <a href="">
+                  <SlSocialFacebook />
+                </a>
+              </li>
+              <li className="hover:bg-blue-400 p-[6px] rounded-full">
+                <a href="">
+                  <SlSocialInstagram className="w-[18px] " />
+                </a>
+              </li>
+              <li className="hover:bg-blue-400 p-[6px] rounded-full">
+                <a href="">
+                  <SlSocialYoutube />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
